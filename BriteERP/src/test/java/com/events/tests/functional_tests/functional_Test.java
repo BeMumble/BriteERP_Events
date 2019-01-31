03868a3d3dd7f3038205a8f4858b12538db17f35
fahim <fahim@LAPTOP-AMJ6T7PB>
2019-01-30 19:05:41 -0500

    Updated packages and added test classes

--- BriteERP/src/test/java/com/events/tests/functional_tests/functional_Test.java
+++
@@ -1,4 +0,0 @@
-package com.events.tests.functional_tests;
-
-public class functional_Test {
-}
---
+++ BriteERP/src/test/java/com/events/tests/pages/searchFunctionalityLocators.java
@@ -0,0 +1,7 @@
+package com.events.tests.pages;
+
+import org.testng.annotations.Test;
+
+public class searchFunctionalityLocators {
+
+}
